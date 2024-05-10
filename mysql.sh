1bec8dfdb5604723c14e7d6ab10ccf6e1df338fa
Khyathi304 <khyathikamireddy04@gmail.com>
2024-05-10 22:23:19 +0100

    expense-shell

--- mysql.sh
+++ mysql.sh
@@ -5,14 +5,18 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2... Failure"
+        echo -e "$2... $R Failure $N"
         exit 1
     else
-        echo "$2... Success"
+        echo -e "$2... $G Success $N"
         fi
 }
 if [ $USERID -ne 0 ]
