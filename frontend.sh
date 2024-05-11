612e590c83c15455cc45251a57d8277583780cbb
Khyathi304 <khyathikamireddy04@gmail.com>
2024-05-11 15:57:04 +0100

    expense-shell

--- frontend.sh
+++ frontend.sh
@@ -33,7 +33,7 @@ VALIDATE $? "Installing nginx"
 systemctl enable nginx &>>$LOGFILE
 VALIDATE $? "Enabling nginx" 
 
-systemctl start nginx 
+systemctl restart nginx 
 VALIDATE $? "starting nginx"
 
 rm -rf /usr/share/nginx/html/*  &>>$LOGFILE
