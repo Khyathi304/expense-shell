695da42417ba2330bce77c9bd340bd897e7daf61
Khyathi304 <khyathikamireddy04@gmail.com>
2024-05-11 15:09:06 +0100

    expense-shell

---
+++ frontend.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2... $R Failure $N"
+        exit 1
+    else
+        echo -e "$2... $G Success $N"
+        fi
+}
+if [ $USERID -ne 0 ]
+then
+echo "Please run the script with root access"
+exit 1
+else
+echo "You are super User"
+fi
+
+dnf install nginx -y 
+VALIDATE $? "Installing Nginx"
+
+systemctl enable nginx
+VALIDATE $? "Enabling Nginx"
+
+systemctl start nginx
+VALIDATE $? "Start Nginx"
+
+rm -rf /usr/share/nginx/html/*
+VALIDATE $? "Removing the content"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+VALIDATE $? "Downloading the content"
+
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+VALIDATE $? "Extracting Content"
+
+cp  /etc/nginx/default.d/expense.conf
