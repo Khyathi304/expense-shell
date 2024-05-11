f515d9e12357738e7c982b2b4c338354b1d624aa
Khyathi304 <khyathikamireddy04@gmail.com>
2024-05-11 16:04:45 +0100

    expense-shell

--- frontendd.sh
+++ frontendd.sh
@@ -32,22 +32,3 @@ VALIDATE $? "Installing nginx"
 
 systemctl enable nginx &>>$LOGFILE
 VALIDATE $? "Enabling nginx" 
-
-systemctl restart nginx 
-VALIDATE $? "restarting nginx"
-
-rm -rf /usr/share/nginx/html/*  &>>$LOGFILE
-VALIDATE $? "Removing default nginx content" 
-
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
-VALIDATE $? "Downloading content"
-
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>>$LOGFILE
-VALIDATE $? "Extracting content"
-
-cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
-VALIDATE $? "Copying content"
-
-
-
