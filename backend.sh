780fb055864059518aa834729fd904025d01bb27
Khyathi304 <khyathikamireddy04@gmail.com>
2024-05-11 14:26:20 +0100

    expense-shell

--- backend.sh
+++ backend.sh
@@ -44,3 +44,15 @@ VALIDATE $? "Adding User "
 else
 echo -e "User already added $Y Skip $N "
 fi
+
+mkdir -p /app &>>$LOGFILE
+VALIDATE $? "Creating the directorty" 
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+VALIDATE $? "Downloading the code"
+
+cd /app
+unzip /tmp/backend.zip 
+VALIDATE $? "Extracting backend code"
+
+npm install
